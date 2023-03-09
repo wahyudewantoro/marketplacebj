35c4ee05653a6be0cac85199ff0fa2aa3cc68b93
Bapenda 205 <bapendda205@gmail.com>
2023-03-09 17:35:24 +0700

    perbaikan

---
+++ config/oracle - Copy.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+
+    'oracle' => [
+        'driver' => 'oracle',
+        'host' => '192.168.1.200',
+        'port' => '1521',
+        'database' => 'spo',
+        'service_name' => 'orcl',
+        'username' => 'spo',
+        'password' => 'B4p3nd4PBB200',
+        'charset' => '',
+        'prefix' => '',
+        'server_version' => '12c',
+    ],
+    'oracle_satutujuh' => [
+        'driver' => 'oracle',
+        'host' => '192.168.1.200',
+        'port' => '1521',
+        'database' => 'pbb',
+        'service_name' => 'orcl',
+        'username' => 'pbb',
+        'password' => 'B4p3nd4PBB200',
+        'charset' => '',
+        'prefix' => '',
+        'server_version' => '12c',
+    ],
+];
